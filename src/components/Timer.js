72bab0077af8432514c9777da487b6e33249c62d
Yuka <yuka1110m@gmail.com>
2017-10-27 20:03:03 +0200

    fixed stylying issue

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import TimerForm from './TimerForm';
-import { addTimer } from '../actions/actionCreator';
 
 const Timer = props => (
   <TimerForm
